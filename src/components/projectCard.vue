d1dde5b8141c6b0e5c4f74bbb4d1bf05ff040951
aniyagnik <ani789yagnik@gmail.com>
2020-01-01 22:11:21 +0530

    portfolio

---
+++ src/components/projectCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='projectcard'>
+    <p class='headline font-weight-bold text-uppercase text-center'>{{ project.name}}</p><br>
+    <p class='subtitle-2 te'>{{ project.summary }}</p>
+    <p class='body-2 font-weight-light'><a :href="`${ project.link }`">{{ project.link }}</a></p>
+    <p class='body-2 font-weight-light'><a :href="`${ project.github }`">{{ project.github }}</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'project' ]
+}
+</script>
+<style lang='scss'>
+.projectcard {
+    background-color: aliceblue;
+    padding: 2vw;
+    padding-top: 3vw;
+    width: 32vw;
+    height: 21vw;
+    margin:5vw;
+    border: 10px solid black;
+}
+.projectcard:hover{
+    background-image: url('../assets/SNS.jpg');
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    transition: all 1s easy 0s;
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,6 +1,11 @@
 <template>
   <div id='portfolio'>
     <h1>This is Portfolio Page</h1>
+    <div id='flex'>
+      <div class="flex-item" v-for='(project,i) in info' :key='i'>
+        <project-card :project='project'></project-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +16,52 @@
   background-position: center;
   background-repeat: no-repeat;
 }
+#flex{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: auto;
+}
 </style>
+
+<script>
+import projectCard from '../components/projectCard'
+export default {
+  components: {
+    'project-card': projectCard
+  },
+  data () {
+    return {
+      info: [
+        {
+          name: 'Social Networking Site',
+          image: 'SNS.jpg',
+          summary: 'A SNS (Social Networking Site) with functionalities sign-up/login, uploading posts, viewing user, web-lock etc.',
+          link: 'https://project1-19.herokuapp.com',
+          github: 'https://aniyagnik/nodejsproject19.git'
+        },
+        {
+          name: 'quiz',
+          summary: '',
+          link: '',
+          github: ''
+        },
+        {
+          name: 'chat app',
+          summary: '',
+          link: '',
+          github: ''
+        },
+        {
+          name: 'cosmos',
+          summary: '',
+          link: '',
+          github: ''
+        }
+      ]
+    }
+  }
+}
+</script>
